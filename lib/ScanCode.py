fcc502ac41b1786c4987bd0c6dbec8705850e069
tdccj <tudouccj@163.com>
2023-06-13 12:08:14 +0800

    修改 create_code 以适应小体积打印机

--- lib/ScanCode.py
+++ lib/ScanCode.py
@@ -11,8 +11,21 @@ class SC:
         self.db = DB(_db)
         self.db.create_table(_table)
 
+    # 创建二维码
     def create_code(self, _db, _table, _id):
-        _text = f"SUAIM/{_db}/{_table}" + str(self.db.show_data_id(_id)[:-2]+(self.db.show_data_id(_id)[-1],))
-        _img = qrcode.make(_text)
+        _text = f"SUAIM/{_db}/{_table}" + str(self.db.show_data_id(_id)[:-2] + (self.db.show_data_id(_id)[-1],))
+
+        # 创建实例
+        qr = qrcode.QRCode(version=2,
+                           error_correction=qrcode.constants.ERROR_CORRECT_M,
+                           box_size=200,
+                           border=4)
+        # 添加文本
+        qr.add_data(_text)
+        # 创建qrcode
+        qr.make(fit=True)
+        _img = qr.make_image()
         with open("qrcode.png", "wb") as q:
             _img.save(q)
+
+    # 创建打印标签
