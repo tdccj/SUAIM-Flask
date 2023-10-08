8d7890fe1e1b39d953f9ccdc13054eeb06338d97
tdccj <tudouccj@163.com>
2023-10-08 21:46:56 +0800

    剪除不需要的quote库以及语句

--- no_gui.py
+++ no_gui.py
@@ -1,7 +1,6 @@
 # coding = utf-8
 import openpyxl
 import requests
-from urllib.parse import quote
 
 xlsx_file = input("Enter the name of the file: ")
 db = "SAIUM_TDhome_test.db"  # 数据库名称
@@ -53,4 +52,4 @@ print(response.text)
 
 # todo 基本验证通过
 # 第二列从现在开始用于type
-# 从现在开始不再对输入数据库的中文进行编码&打印标签的id可以正常显示中文了(把quote()放到了打印前面，而不是写入前面)
+# 从现在开始不再对输入数据库的中文进行编码&打印标签的id可以正常显示中文了
