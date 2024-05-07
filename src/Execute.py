1226f7ca89f55c3b459e697510d14c895f015772
tdccj <tudouccj@163.com>
2024-05-07 18:07:35 +0800

    v0.0.1-4

---
+++ src/Execute.py
@@ -0,0 +1,13 @@
+# coding = utf-8
+
+# ------------------------------
+# Execute 模块用于 DBX 数据库操作，
+# 尤其是简化 DBX 的异常处理步骤。
+# ------------------------------
+
+def execute(query, handle, commit=False, ignore=None, *args):
+    # 执行查询语句    query
+    # 方法名    handle
+    # 提交事务  commit=True
+    # 忽略的异常类型   ignore
+    pass
