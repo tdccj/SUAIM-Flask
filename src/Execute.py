78c59bab8e0f271541c5f3839e4f920e4903fd59
tdccj <tudouccj@163.com>
2024-05-07 20:24:22 +0800

    v0.0.1-4

--- src/Execute.py
+++ src/Execute.py
@@ -1,13 +1,34 @@
 # coding = utf-8
+from sqlite3 import Connection
+
+from src import Logger
+
 
 # ------------------------------
 # Execute 模块用于 DBX 数据库操作，
 # 尤其是简化 DBX 的异常处理步骤。
 # ------------------------------
 
-def execute(query: str, handle: str, commit: bool = False, ignore: list = None):
+
+def execute(conn: Connection, query: str, handle: str, log: Logger, commit: bool = False, ignore: list = None):
     # 执行查询语句    query
     # 方法名    handle
     # 提交事务  commit=True
     # 忽略的异常类型   ignore
-    pass
+    try:
+
+        conn.cursor().execute("")
+
+        conn.commit()  # 提交
+
+        self.log.debug(f"Create to table '{table_name}' successfully")
+
+        return {"status": "success", "message": f"Create table '{table_name}' successfully"}
+
+    except sqlite3.OperationalError as ex:
+        if "already exists" in str(ex):
+            self.log.info(f"Create table '{table_name}' failed , Because '{ex}'")
+            return {"status": "failed", "message": f"Table '{table_name}' is already exists"}
+        else:
+            self.log.warning(f"Create table '{table_name}' failed , Because '{ex}'")
+            return {"status": "failed", "message": f"Create table {table_name} failed"}
