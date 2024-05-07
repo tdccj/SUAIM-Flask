e0755d09fe5968d53e62e6395659c5fb64497130
tdccj <tudouccj@163.com>
2024-05-07 14:42:54 +0800

    DBX create_table is ok

--- src/DatabaseX.py
+++ src/DatabaseX.py
@@ -6,23 +6,65 @@
 #   ——————————————————————————————————————————————————————————
 
 import sqlite3
-from Logger import logger
+from src.Logger import logger
 
 
-class DB:
+class DBX:
     def __init__(self, path):
-        log = logger("DatabaseX")   # 创建日志记录
+        self.log = logger("DatabaseX")  # 创建日志记录
 
         self.path = "../database/" + path
 
         self.conn = sqlite3.connect(self.path)  # 连接数据库
 
-        log.debug("Connect to database successfully")
+        self.log.debug("Connect to database successfully")
 
         self.cursor = self.conn.cursor()  # 创建游标
 
         self.columns = ["id", "name", "type", "tag", "quantity", "price", "consumables", "remark", "ascription"]
 
+    def get_normal_columns(self):
+        # 获取默认列名
+        return self.columns
 
-if __name__ == '__main__':
-    db = DB("test.db")
+    def create_table(self, table_name):
+        # 创建表单
+
+        #   ________________________________________
+        #   name        INTEGER 名称
+        #   type        TEXT    类型
+        #   tag         TEXT    标签
+        #   quantity    REAL    数量
+        #   price       REAL    价值
+        #   consumables TEXT    是否为消耗品(消耗品周期)
+        #   remark      TEXT    备注
+        #   ascription  TEXT    归属人
+        #   ________________________________________
+
+        try:
+            self.cursor.execute(f'''CREATE TABLE "{table_name}" (
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            name        TEXT    NOT NULL,
+            type        TEXT    NOT NULL,
+            tag         TEXT,
+            quantity    REAL    NOT NULL,
+            price       REAL,
+            consumables TEXT,   
+            remark      TEXT,
+            ascription  TEXT    NOT NULL
+
+
+            );''')
+
+            self.conn.commit()  # 提交
+
+            self.log.debug(f"Create to table {table_name} successfully")
+
+            return {"status": "success", "message": f"Create to table {table_name} successfully"}
+
+        except sqlite3.OperationalError as ex:
+            self.log.debug(f"Create to table {table_name} failed , Because {ex}")
+            if "already exists" in str(ex):
+                return {"status": "failed", "message": f"Table {table_name} is already exists"}
+            else:
+                return {"status": "failed", "message": f"Create to table {table_name} failed"}
